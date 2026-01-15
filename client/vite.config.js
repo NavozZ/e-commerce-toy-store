8446425ae44fc4855b90f102d02ba3eff9dc06ad
NavozZ <navodyatheshan4@gmail.com>
2026-01-15 21:28:10 +0530

    Refactor LiveFeed component to use Vite Proxy for socket connection, add BestSellers component to Home page, and implement best sellers API route

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'; 
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),],
   server: {
     proxy: {
+      // Bridging the frontend (5173) to the backend (5000)
       '/api': {
-        target: 'http://localhost:5000', // Redirects client calls to your backend
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
+      // Bridging WebSockets for Member 4
       '/socket.io': {
         target: 'http://localhost:5000',
         ws: true,
       },
-    }
-  }
-})
+    },
+  },
+});
