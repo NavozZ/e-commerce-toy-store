1cb22bd24285d4472c47a1a02758d40e2f75e55d
dimethma <ranaweeradimethma@gmail.com>
2026-01-15 23:24:01 +0530

    Refactor Home component layout for improved structure and readability

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,19 +5,27 @@ import Hero from '../components/Hero';
 import Categories from '../components/Categories';
 import About from '../components/About';
 import BestSellers from '../components/BestSellers';
+import LiveFeed from '../components/LiveFeed';
 
 
 const Home = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
-      {/* Welcome Section - Hero Component */}
-      <Hero />
-      <Categories />
-      <About />
-      <BestSellers />
+    <main className="min-h-screen bg-white">
 
-      
-    </div>
+      <div className="max-w-350 mx-auto px-4 md:px-8 space-y-20 pb-20">
+        {/* Welcome & Brand Story [Member 1 & 6] */}
+        <Hero />
+        
+        {/* Navigation by Categories [Member 5] */}
+        <Categories />
+        
+        {/* Brand Mission [Leader] */}
+        <About />
+        
+        {/* Dynamic Database Products [Member 1] */}
+        <BestSellers />
+      </div>
+    </main>
   );
 };
 
